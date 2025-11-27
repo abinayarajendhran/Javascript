e0df0ef5a3ccfeab48508ead025d2f7969601367
Abinaya <abinayaabanumathy@gmail.com>
2025-11-27 15:19:58 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,21 @@
+let a=90;
+if(a>90){
+    console.log("S Grade");
+}
+else if(a>80 && a<=90){
+    console.log("A Grade");
+}
+else if(a>70 && a<=80){
+    console.log("B Grade");
+}
+else{
+    console.log("Not eligible");
+}
+let age=20;
+if(age>=18){
+    console.log("ELIGIBLE FOR VOTE");
+}
+else{
+    console.log("NOT ELIGIBLE FOR VOTE");
+}
+    
